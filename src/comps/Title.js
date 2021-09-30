4dc9024cf5ee75c085bcf79c405c8df362954c50
Timilehin08 <52857292+Timilehin08@users.noreply.github.com>
2021-10-01 00:20:13 +0100

    First commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>FogoGram</h1>
+      <h2>Your Pictures</h2>
+      <p>A Click Of Artistic Joy With Captures Of Flawless Moments.</p>
+    </div>
+  )
+}
+
+export default Title;

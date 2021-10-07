67d3b7ee5f931abf87af21f9f7dbde662b521a0b
Timilehin08 <52857292+Timilehin08@users.noreply.github.com>
2021-10-07 04:02:11 +0100

    name caption

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FogoGram</h1>
-      <h2>Your Pictures</h2>
+      <h1>NeilShots.</h1>
+      <h2>NeilShots.</h2>
       <p>A Click Of Artistic Joy With Captures Of Flawless Moments.</p>
     </div>
   )

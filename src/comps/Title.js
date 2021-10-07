8137caae1f8797fbd29862924b87beaf9282194d
Timilehin08 <52857292+Timilehin08@users.noreply.github.com>
2021-10-07 04:08:36 +0100

    changed logo

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>NeilShots.</h1>
+      <h1>FogoGram</h1>
       <h2>NeilShots.</h2>
       <p>A Click Of Artistic Joy With Captures Of Flawless Moments.</p>
     </div>
